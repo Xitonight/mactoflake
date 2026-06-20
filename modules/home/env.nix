5282ea1bfc6059b9624d635f3e167e8329033212
Xitonight <xitonight@gmail.com>
2026-06-20 05:31:39 +0200

    feat(shell): add env vars

---
+++ modules/home/env.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  xdg.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    MANROFFOPT = "-c";
+    BAT_THEME = "base16";
+    GOPATH = "$HOME/.go";
+    ANDROID_HOME = "$HOME/.Android/Sdk";
+    PNPM_HOME = "$HOME/.local/share/pnpm";
+  };
+
+  home.sessionPath = [
+    "$HOME/.local/share/pnpm"
+    "$HOME/.go/bin"
+    "$HOME/.local/bin"
+  ];
+}
