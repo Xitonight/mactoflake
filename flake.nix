62b403c4ba2012a0caf477af8375c30b67a5f32e
Xitonight <xitonight@gmail.com>
2026-06-22 01:06:52 +0200

    style: fix formatting

--- flake.nix
+++ flake.nix
@@ -14,27 +14,25 @@
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
-  outputs =
-    { self, nixpkgs, home-manager, ... }@inputs:
-    {
-      nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./hosts/vm
-          ./modules/system
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/vm
+        ./modules/system
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.xitonight = import ./modules/home;
-            };
-          }
-        ];
-      };
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            # Expose flake inputs (e.g. zen-browser) to Home Manager modules.
+            extraSpecialArgs = { inherit inputs; };
+            users.xitonight = import ./modules/home;
+          };
+        }
+      ];
     };
+  };
 }
