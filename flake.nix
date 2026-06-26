67d7004c623be02611a0f958875c5c6ac5dd55e9
Xitonight <xitonight@gmail.com>
2026-06-26 07:41:38 +0200

    feat: reimplement zen browser home-manager setup

--- flake.nix
+++ flake.nix
@@ -6,13 +6,23 @@
     minegrub-theme.url = "github:Lxtharia/minegrub-theme";
     hyprland.url = "github:hyprwm/Hyprland";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     hjem = {
       url = "github:feel-co/hjem";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -21,6 +31,16 @@
         ./hosts/vm
         ./modules/system
         inputs.minegrub-theme.nixosModules.default
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs; };
+            users.xitonight = import ./modules/home;
+          };
+        }
       ];
     };
   };
---
+++ modules/home/default.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }:
+
+{
+  home.username = "xitonight";
+  home.homeDirectory = "/home/xitonight";
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+
+  imports = [ ./zen.nix ];
+}
