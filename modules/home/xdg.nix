0bae37e98ea57de96b73ce02f871a894930fd00f
Xitonight <xitonight@gmail.com>
2026-06-28 09:14:40 +0200

    feat: add xdg user dirs module

---
+++ modules/home/xdg.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      download = "dl";
+      pictures = "pics";
+      documents = "docs";
+    };
+  };
+}
