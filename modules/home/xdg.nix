d03ed4b7ff619085ddd8f4d112af0d3315ae3bd5
Xitonight <xitonight@gmail.com>
2026-07-02 14:34:15 +0200

    feat(xdg): add videos dir and set env vars

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,14 +1,15 @@
 { ... }:
-
 {
   xdg = {
     userDirs = {
       enable = true;
+      setSessionVariables = true;
       createDirectories = true;
       download = "dl";
       pictures = "pics";
       documents = "docs";
       projects = "projects";
+      videos = "videos";
     };
   };
 }
