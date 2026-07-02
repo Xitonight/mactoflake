85cbb09ca39c34b07375f3e2c73c2ae5bea63a41
Xitonight <xitonight@gmail.com>
2026-07-03 00:56:35 +0200

    feat(boot): enable silent boot

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -30,10 +30,10 @@ in {
     {
       boot = {
         loader.efi.canTouchEfiVariables = true;
-        # consoleLogLevel = 0;
-        # kernelParams = ["quiet" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"];
-        # initrd.verbose = false;
-        # initrd.systemd.enable = true;
+        consoleLogLevel = 0;
+        kernelParams = ["quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"];
+        initrd.verbose = false;
+        initrd.systemd.enable = true;
       };
     }
   ];
