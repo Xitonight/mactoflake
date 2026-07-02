28db1a718e42ca26cee37fe8043bf3d5a378c10a
Xitonight <xitonight@gmail.com>
2026-07-02 20:44:39 +0200

    fix(xdg): fix all user dir paths

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,15 +1,22 @@
-{ ... }:
+{ config, ... }:
+let
+  hd = config.home.homeDirectory;
+in
 {
   xdg = {
     userDirs = {
       enable = true;
       setSessionVariables = true;
       createDirectories = true;
-      download = "dl";
-      pictures = "pics";
-      documents = "docs";
-      projects = "projects";
-      videos = "videos";
+      download = "${hd}/dl";
+      pictures = "${hd}/pics";
+      documents = "${hd}/docs";
+      projects = "${hd}/projects";
+      videos = "${hd}/videos";
+      publicShare = "${hd}";
+      templates = "${hd}";
+      music = "${hd}";
+      desktop = "${hd}";
     };
   };
 }
