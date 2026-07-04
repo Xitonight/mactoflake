ff3549285a0cc09e98ca0b233ab7388ed14a8de7
Xitonight <xitonight@gmail.com>
2026-07-05 01:20:29 +0200

    fix(zsh/sesh): remove -t -c flags to list all sessions

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -195,7 +195,7 @@
               exec </dev/tty
               exec <&1
               local session
-              session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+              session=$(sesh list | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
               zle reset-prompt >/dev/null 2>&1 || true
               [[ -z "$session" ]] && return
               sesh connect $session
