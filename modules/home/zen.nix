696da7be99d9c7222ca52a842cff4df3e73507db
Xitonight <xitonight@gmail.com>
2026-07-15 16:18:52 +0200

    feat(zen): improve spaces/pins setup

--- modules/home/zen.nix
+++ modules/home/zen.nix
@@ -5,33 +5,6 @@ let
     inherit id;
     disabled = true;
   });
-  spaces = {
-    "Uni" = {
-      id = "a6de093b-408d-4206-961d-ab11f989d41b";
-      position = 1000;
-      icon = "🎓";
-    };
-    "Dots" = {
-      id = "edd11fae-1fc5-494b-9041-325e5759198c";
-      position = 2000;
-      icon = "💠";
-    };
-    "Default" = {
-      id = "8015cee6-9f50-4dc1-8ecf-baa25f8c8fd7";
-      position = 3000;
-      icon = "🩷";
-    };
-    "Projects" = {
-      id = "beb980ca-fcde-38ba-9d54-04a84569e2c3";
-      position = 4000;
-      icon = "🌙";
-    };
-    "Work" = {
-      id = "decf5613-69b4-4e57-927a-68ad8068808e";
-      position = 5000;
-      icon = "🗂";
-    };
-  };
 in
 {
   imports = [ inputs.zen-browser.homeModules.beta ];
@@ -116,8 +89,119 @@ in
         ];
       };
 
+      containers = {
+        Personal = {
+          color = "purple";
+          icon = "fingerprint";
+          id = 1;
+        };
+        Uni = {
+          color = "blue";
+          icon = "briefcase";
+          id = 2;
+        };
+        Demetra = {
+          color = "blue";
+          icon = "briefcase";
+          id = 3;
+        };
+        Sedapta = {
+          color = "yellow";
+          icon = "briefcase";
+          id = 4;
+        };
+      };
+
       spacesForce = true;
-      inherit spaces;
+      spaces = {
+        "Uni" = {
+          id = "a6de093b-408d-4206-961d-ab11f989d41b";
+          position = 1000;
+          icon = "🎓";
+          container = 2;
+          pins = {
+            "AulaWeb" = {
+              id = "28a03b59-e235-4b1b-9dac-e484d7d9d510";
+              url = "https://2025.aulaweb.unige.it/my/";
+            };
+            "Gemini" = {
+              id = "4df0c698-8616-4b1e-bd18-d5422cb4630a";
+              url = "https://gemini.google.com/app";
+            };
+          };
+        };
+        "Dots" = {
+          id = "edd11fae-1fc5-494b-9041-325e5759198c";
+          position = 2000;
+          icon = "💠";
+          container = 1;
+          pins = {
+            "My-GitHub" = {
+              id = "f74716f2-9475-486b-8321-9c23e2941c2d";
+              url = "https://github.com/Xitonight?tab=repositories";
+            };
+          };
+        };
+        "Default" = {
+          id = "8015cee6-9f50-4dc1-8ecf-baa25f8c8fd7";
+          position = 3000;
+          icon = "🩷";
+          container = 1;
+        };
+        "Projects" = {
+          id = "beb980ca-fcde-38ba-9d54-04a84569e2c3";
+          position = 4000;
+          icon = "🌙";
+          container = 1;
+        };
+        "Work" = {
+          id = "decf5613-69b4-4e57-927a-68ad8068808e";
+          position = 5000;
+          icon = "🗂";
+          pins = {
+            "Demetra" = {
+              id = "2898cb7f-bee0-4185-99a9-f60a507fa7e6";
+              isFolderCollapsed = false;
+              editedTitle = true;
+              position = 200;
+              pins = {
+                "Teams" = {
+                  id = "1a223ccc-25b0-43e1-a3a5-3bd5e3e8cf40";
+                  url = "https://teams.cloud.microsoft/";
+                  container = 3;
+                  position = 201;
+                };
+                "Outlook" = {
+                  id = "28c0d9e7-dfb2-42c1-aa91-65e5744277db";
+                  url = "https://outlook.office.com/mail/";
+                  container = 3;
+                  position = 202;
+                };
+              };
+            };
+            "Sedapta" = {
+              id = "7b0650a5-0e2f-480f-966d-ffe397973037";
+              isFolderCollapsed = false;
+              editedTitle = true;
+              position = 300;
+              pins = {
+                "Teams" = {
+                  id = "3f6eec3a-0862-42ed-8a70-41d0a3181f65";
+                  url = "https://teams.cloud.microsoft/";
+                  container = 4;
+                  position = 301;
+                };
+                "Outlook" = {
+                  id = "617552bd-f56c-4aa6-babf-6b919b756a07";
+                  url = "https://outlook.office.com/mail/";
+                  container = 4;
+                  position = 302;
+                };
+              };
+            };
+          };
+        };
+      };
 
       pinsForce = true;
       pinsForceAction = "remove";
@@ -128,38 +212,17 @@ in
           position = 101;
           isEssential = true;
         };
-        "GitHub" = {
-          id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
-          url = "https://github.com";
-          position = 102;
-          isEssential = true;
-        };
         "Gmail" = {
           id = "682fa99f-a637-47f5-addd-71564cc5ed74";
           url = "https://mail.google.com";
-          position = 103;
+          position = 102;
           isEssential = true;
         };
         "Gemini" = {
           id = "57c4cf02-c422-4631-bbc8-4fade8d1b883";
           url = "https://gemini.google.com/app";
-          position = 104;
+          position = 103;
           isEssential = true;
-        };
-        "AulaWeb" = {
-          id = "28a03b59-e235-4b1b-9dac-e484d7d9d510";
-          url = "https://2025.aulaweb.unige.it/my/";
-          workspace = spaces."Uni".id;
-        };
-        "Teams" = {
-          id = "1a223ccc-25b0-43e1-a3a5-3bd5e3e8cf40";
-          url = "https://teams.cloud.microsoft/";
-          workspace = spaces."Work".id;
-        };
-        "Outlook" = {
-          id = "28c0d9e7-dfb2-42c1-aa91-65e5744277db";
-          url = "https://outlook.office.com/mail/";
-          workspace = spaces."Work".id;
         };
       };
 
