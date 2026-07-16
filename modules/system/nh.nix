200e58df569b2b4b9d6c959ab302fea02dcc465d
Xitonight <xitonight@gmail.com>
2026-07-16 11:59:04 +0200

    style: comment some stuff and improve formatting and linting

--- modules/system/nh.nix
+++ modules/system/nh.nix
@@ -4,7 +4,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep-since 3d --keep 3";
     flake = "${flakeDir}"; # sets NH_OS_FLAKE variable for you
   };
 }
--- modules/system/udev.nix
+++ modules/system/udev.nix
@@ -1,12 +1,13 @@
 { username, ... }:
 
 {
-  users.groups.plugdev = {};
+  users.groups.plugdev = { };
 
+  # Fixes silakka54 not being recognized
   services.udev.extraRules = ''
-  SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE="0666", GROUP="plugdev", TAG+="uaccess"
-  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE="0666", GROUP="plugdev", TAG+="uaccess"
-'';
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE="0666", GROUP="plugdev", TAG+="uaccess"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE="0666", GROUP="plugdev", TAG+="uaccess"
+  '';
   users.users."${username}" = {
     extraGroups = [
       "input"
