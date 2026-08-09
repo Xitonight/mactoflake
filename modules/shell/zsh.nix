0c6b586b6912fcb64aefe90927ff846f335ee586
Xitonight <xitonight@gmail.com>
2026-08-09 14:01:28 +0200

    feat(zsh): add fastfetch alias

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -72,6 +72,7 @@
           v = "nvim";
           vim = "nvim";
           j = "just";
+          ff = "fastfetch";
           oc = "opencode --port";
           open = "xdg-open";
           rsy = "rsync -ahP";
