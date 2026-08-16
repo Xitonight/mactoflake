840425f2d76f415be729519f1e385a8214d8af87
Xitonight <xitonight@gmail.com>
2026-08-16 12:27:38 +0200

    feat(ssh): add authorized keys

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -3,6 +3,14 @@
     { username, ... }:
     {
       networking.networkmanager.enable = true;
+      users.users."${username}" = {
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVODpv0S1p5R9fCHeEy8AZTHjnFuVdB3UN6CNlyGuOt mactone"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOx7KW5d4Xtx3fvBDCSeBylB5hTPYIzMB/ss7qJwva/ mactopad"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5VCATBzbOJajXJSgW8OvBiSn6MOB5vGttNBtKSXlKG mactoncino"
+        ];
+        extraGroups = [ "networkmanager" ];
+      };
 
       services.openssh = {
         enable = true;
