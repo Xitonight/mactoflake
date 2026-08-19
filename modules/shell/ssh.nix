5bec041d174d0130f17097723eab410d353519d3
Xitonight <xitonight@gmail.com>
2026-08-19 15:11:57 +0200

    feat(ssh): implement agent forwarding

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -1,17 +1,28 @@
 {
   flake.homeModules.ssh =
-    { config, ... }:
+    {
+      config,
+      lib,
+      osConfig,
+      ...
+    }:
     let
       onePassPath = "${config.home.homeDirectory}/.1password/agent.sock";
+      forwardToMactone = osConfig != null && osConfig.networking.hostName != "mactone";
     in
     {
-      home.sessionVariables.SSH_AUTH_SOCK = onePassPath;
+      home.sessionVariables.SSH_AUTH_SOCK = "\${SSH_AUTH_SOCK:-${onePassPath}}";
 
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
-        settings."*" = {
-          IdentityAgent = onePassPath;
+        settings = {
+          "Match host * exec \"test -z \$SSH_TTY\"" = {
+            IdentityAgent = onePassPath;
+          };
+        }
+        // lib.optionalAttrs forwardToMactone {
+          mactone.ForwardAgent = true;
         };
       };
     };
