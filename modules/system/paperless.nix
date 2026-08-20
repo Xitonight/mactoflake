cf847cc7f6265c89f1006ee7929aab36fa91a40f
Xitonight <xitonight@gmail.com>
2026-08-20 20:36:52 +0200

    fix(paperless): fix unoconverter wrong package issue to fix docx compatibility

--- modules/system/paperless.nix
+++ modules/system/paperless.nix
@@ -1,6 +1,32 @@
 {
   flake.nixosModules.paperless =
-    { config, ... }:
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      unoconverter = pkgs.stdenv.mkDerivation {
+        pname = "unoconverter";
+        version = "0.4.0";
+        src = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/gotenberg/unoconverter/v0.4.0/unoconv";
+          hash = "sha256-RqVGO8iAYkfuIjvk4pVORPCWGq28SWDliaOpJeoNLSs=";
+        };
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        dontUnpack = true;
+        installPhase = ''
+          install -Dm755 $src $out/bin/unoconverter
+          sed -i "1s|.*|#!${pkgs.libreoffice-unwrapped.python.interpreter}|" $out/bin/unoconverter
+          substituteInPlace $out/bin/unoconverter \
+            --replace-fail "from distutils.version import LooseVersion" "" \
+            --replace-fail "or LooseVersion(product.ooSetupVersion) <= LooseVersion('3.3')" "or False"
+          wrapProgram $out/bin/unoconverter \
+            --set-default UNO_PATH "${pkgs.libreoffice-unwrapped}/lib/libreoffice/program/"
+        '';
+        meta.mainProgram = "unoconverter";
+      };
+    in
     {
       services.paperless = {
         enable = true;
@@ -12,10 +38,14 @@
           PAPERLESS_FORCE_SCRIPT_NAME = "/paperless";
           PAPERLESS_STATIC_URL = "/paperless/static/";
           PAPERLESS_CSRF_TRUSTED_ORIGINS = [ "https://mactoncino.taila7373f.ts.net" ];
+          PAPERLESS_CONSUMER_DELETE_DUPLICATES = true;
         };
       };
 
-      services.gotenberg.port = 3199;
+      services.gotenberg = {
+        port = 3199;
+        package = pkgs.gotenberg.override { unoconv = unoconverter; };
+      };
 
       sops.secrets.paperless-password.sopsFile = ../../secrets/paperless.yaml;
 
