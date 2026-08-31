fac3e53b565ac6a07bb027d531cf8658b144d4bb
Xitonight <xitonight@gmail.com>
2026-08-31 08:29:35 +0200

    feat(homeassistant): add needed packages for lightbulb integrations

--- modules/system/home-assistant.nix
+++ modules/system/home-assistant.nix
@@ -1,20 +1,42 @@
 {
-  flake.nixosModules.home-assistant = {
-    services.home-assistant = {
-      enable = true;
-      extraComponents = [
-        "default_config"
-        "pi_hole"
-        "met"
-      ];
-      config = {
-        homeassistant = {
-          name = "mactoncino";
-          latitude = 41.9028;
-          longitude = 12.4964;
-          unit_system = "metric";
+  flake.nixosModules.home-assistant =
+    { pkgs, ... }:
+    {
+      services.home-assistant = {
+        enable = true;
+        package =
+          (pkgs.home-assistant.override {
+            extraPackages = ps: [
+              ps.isal
+              ps.zlib-ng
+            ];
+          }).overrideAttrs
+            (oldAttrs: {
+              doInstallCheck = false;
+            });
+        extraComponents = [
+          "default_config"
+          "pi_hole"
+          "met"
+          "tplink"
+        ];
+        config = {
+          homeassistant = {
+            name = "mactoncino";
+            latitude = 41.9028;
+            longitude = 12.4964;
+            unit_system = "metric";
+          };
+          automation = "!include automations.yaml";
+          scene = "!include scenes.yaml";
+          script = "!include scripts.yaml";
         };
       };
+
+      systemd.tmpfiles.rules = [
+        "f /var/lib/hass/automations.yaml 0600 hass hass - []"
+        "f /var/lib/hass/scenes.yaml 0600 hass hass - []"
+        "f /var/lib/hass/scripts.yaml 0600 hass hass - []"
+      ];
     };
-  };
 }
