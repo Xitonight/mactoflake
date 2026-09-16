b9996411c3b119b07520a8437df98a96ab70a153
Xitonight <xitonight@gmail.com>
2026-09-16 16:24:21 +0200

    chore(devenv): drop unused packages and sync script

--- devenv.nix
+++ devenv.nix
@@ -5,27 +5,9 @@
   inputs,
   ...
 }:
-
 {
-  # https://devenv.sh/packages/
-  packages = with pkgs; [
-    git
-  ];
-
   git-hooks.hooks = {
     statix.enable = true;
     nixfmt.enable = true;
-  };
-
-  # https://devenv.sh/scripts/
-  scripts = {
-    sync.exec = ''
-      set -euo pipefail
-
-      host="''${1:-mactoncino}"
-
-      echo "==> Syncing flake checkout on $host"
-      ssh "$host" 'git -C ~/.mactoflake pull --ff-only'
-    '';
   };
 }
