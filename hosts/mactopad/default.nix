cf931d5939811fd61a4f0bb9734def8ebe32a266
Xitonight <xitonight@gmail.com>
2026-09-16 16:24:21 +0200

    chore(mactopad): enable DP-1 monitor and reposition workspaces

--- hosts/mactopad/default.nix
+++ hosts/mactopad/default.nix
@@ -68,11 +68,10 @@
               position = "-1920x0";
             }
             {
-              disabled = true;
               output = "DP-1";
               mode = "1920x1080@75";
               scale = 1;
-              position = "0x0";
+              position = "1920x0";
             }
             {
               output = "HDMI-A-1";
